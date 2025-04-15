986419a96a63f84a8ff64498394ce7467431d7f3
SpicyCode <contact@behih.fr>
2025-04-15 16:33:54 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let nav = [
+    "Franck,", "Nami", "Sanji", "Chopper", "Robin", "Usopp"
+];
+
+let desert = [
+    "eclair au chocolat",
+    "profiterole",
+    "tarte aux pommes",
+    "fraisier",
+    "religieuse",
+    "tiramisu",
+    "creme brulee",
+    "croissant",
+    "mille-feuille",
+    "paris-brest"
+]
+
+
+desert.forEach((element, index) => console.log(`${index} ${element}`))
+
+
